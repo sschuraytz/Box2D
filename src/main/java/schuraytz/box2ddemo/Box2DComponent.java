363229d4f2cea9fa7914348b6eec6e573e338cdf
sschuraytz <sbschuraytz@gmail.com>
2019-11-14 03:17:29 -0500

    created a box with 3 balls, each of which is a different size and has a different velocity. The balls do not actually touch even when bouncing against each other because Box2D leaves a buffer space.

--- src/main/java/schuraytz/box2ddemo/Box2DComponent.java
+++ src/main/java/schuraytz/box2ddemo/Box2DComponent.java
@@ -14,66 +14,61 @@ import java.awt.*;
 
 public class Box2DComponent extends JComponent {
 
-    private long time;
-
     private final World world;
-    private Body ball;
-    private Body ground;
- // private Body box;
+    private PolygonShape shape;
+    private Body ball1;
+    private Body ball2;
+    private Body ball3;
+    private long time;
+    private final int BOX_WIDTH = 200;
+    private final int BOX_MARGIN = 10;
 
     public Box2DComponent() {
         World.setVelocityThreshold(0);
         world = new World(new Vector2(0, 9.8f), false);
+        shape = new PolygonShape();
 
-        createGround();
-        createBall();
-      //createBox();
-
+        createAllWalls();
+        ball1 = createBall(10);
+        ball2 = createBall(20);
+        ball3 = createBall(30);
         time = System.currentTimeMillis();
-
+    }
+    
+    private void createAllWalls() {
+        createWall(BOX_WIDTH, 0, 0, 0);     //top wall
+        createWall(0, BOX_WIDTH, 0, 0);     //left wall
+        createWall(BOX_MARGIN, BOX_WIDTH, BOX_WIDTH, 0);  //right wall
+        createWall(BOX_WIDTH, 0, 0, BOX_WIDTH);   //bottom wall
     }
 
-    private void createGround() {
+    private void createWall(int hx, int hy, int x, int y) {
         BodyDef bodyDef = new BodyDef();
         bodyDef.type = BodyType.StaticBody;
-        bodyDef.position.set(-100, 200f);
-        ground = world.createBody(bodyDef);
+        Body boxBody = world.createBody(bodyDef);
 
         FixtureDef fixtureDef = new FixtureDef();
-        PolygonShape shape = new PolygonShape();
-        shape.setAsBox(200f, 1f);       // a physics simulation shouldn't necessarily depend on the dimensions of the screen
         fixtureDef.shape = shape;
-        fixtureDef.restitution = 1;     //makes it bounce off of the ground
-        ground.createFixture(fixtureDef);
+        fixtureDef.restitution = 1;
+        shape.setAsBox(hx, hy, new Vector2(x, y), 0);
+        boxBody.createFixture(fixtureDef);
     }
 
-    private void createBall() {
+    private Body createBall(int radius) {
         BodyDef bodyDef = new BodyDef();
         bodyDef.type = BodyType.DynamicBody;
-        //next 2 lines cause ball to move in an arc and fall below ground level after first ground hit
-//        bodyDef.angularVelocity = 40;
-//        bodyDef.angle = (float) Math.toRadians(30);
-        ball = world.createBody(bodyDef);
+        bodyDef.position.set(radius, BOX_MARGIN);
+        Body ball = world.createBody(bodyDef);
 
         FixtureDef fixtureDef = new FixtureDef();
         CircleShape circle = new CircleShape();
-        circle.setRadius(20);
+        circle.setRadius(radius);
         fixtureDef.shape = circle;
+        fixtureDef.restitution = 1;
         ball.createFixture(fixtureDef);
-
-        ball.applyForceToCenter(200, -30, true);
+        ball.applyForceToCenter(radius*BOX_WIDTH, radius*BOX_WIDTH, true);
+        return ball;
     }
-/*    private void createBox() {
-        BodyDef boxDef = new BodyDef();
-        boxDef.type = BodyType.DynamicBody;
-        box = world.createBody(boxDef);
-
-        FixtureDef fixtureDef = new FixtureDef();
-        PolygonShape shape = new PolygonShape();
-        shape.setAsBox(5, 5);
-        fixtureDef.shape = shape;
-        box.createFixture(fixtureDef);
-    }*/
 
     @Override
         protected void paintComponent(Graphics graphics) {
@@ -83,9 +78,19 @@ public class Box2DComponent extends JComponent {
             world.step((currentTime - time) / 1000f, 6, 2); //amount of millis since last time called paintComponent
             time = currentTime;
 
-            // graphics.fillRect((int) box.getPosition().x, (int) box.getPosition().y, 5, 5);    //x, y is location of our body
-            graphics.fillOval((int) ball.getPosition().x, (int) ball.getPosition().y, 20, 20);    //x, y is location of our body
-            graphics.fillRect((int) ground.getPosition().x, (int) ground.getPosition().y, 200, 1);
+            graphics.drawLine(BOX_MARGIN, BOX_WIDTH, BOX_WIDTH - BOX_MARGIN, BOX_WIDTH);    //bottom wall
+            graphics.drawLine(BOX_WIDTH - BOX_MARGIN, BOX_MARGIN, BOX_WIDTH - BOX_MARGIN, BOX_WIDTH);         //right wall
+            graphics.drawLine(BOX_MARGIN, BOX_MARGIN, BOX_MARGIN, BOX_WIDTH);                               //left wall
+            graphics.drawLine(BOX_MARGIN, BOX_MARGIN, BOX_WIDTH - BOX_MARGIN, BOX_MARGIN);                               //top wall
+            drawBall(graphics, ball1, 10);
+            drawBall(graphics, ball2, 20);
+            drawBall(graphics, ball3, 30);
+
             repaint();
+        }
+
+        public void drawBall(Graphics graphics, Body ball, int radius) {
+            graphics.fillOval((int) ball.getPosition().x, (int) ball.getPosition().y,
+                    radius, radius);
         }
 }
