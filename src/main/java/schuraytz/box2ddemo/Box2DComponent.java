32deaefeff0d26a22340fc00392a75cd1b542af4
sschuraytz <sbschuraytz@gmail.com>
2019-11-21 22:17:45 -0500

    created one flipper and added force. still need to figure out how to show graphical depiction of the flipper moving

--- src/main/java/schuraytz/box2ddemo/Box2DComponent.java
+++ src/main/java/schuraytz/box2ddemo/Box2DComponent.java
@@ -4,13 +4,17 @@ import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.physics.box2d.Body;
 import com.badlogic.gdx.physics.box2d.BodyDef;
 import com.badlogic.gdx.physics.box2d.BodyDef.BodyType;
-import com.badlogic.gdx.physics.box2d.CircleShape;
+import com.badlogic.gdx.physics.box2d.ChainShape;
 import com.badlogic.gdx.physics.box2d.PolygonShape;
 import com.badlogic.gdx.physics.box2d.World;
 import com.badlogic.gdx.physics.box2d.FixtureDef;
+import com.badlogic.gdx.physics.box2d.joints.RevoluteJoint;
+import com.badlogic.gdx.physics.box2d.joints.RevoluteJointDef;
 
 import javax.swing.JComponent;
+import javax.swing.event.MouseInputAdapter;
 import java.awt.*;
+import java.awt.event.MouseEvent;
 
 public class Box2DComponent extends JComponent {
 
@@ -19,25 +23,65 @@ public class Box2DComponent extends JComponent {
 
     private final World world;
     private PolygonShape shape;
-    private Body ball1;
-    private Body ball2;
-    private Body ball3;
     private long time;
     private final int BOX_WIDTH = 200;
     private final int BOX_MARGIN = 10;
     private Body boxBody;
+    private Body bodyA;
+    private Body bodyB;
+
+    private Vector2[] staticPoints = new Vector2[]{new Vector2(130, 140), new Vector2(130, 140),
+                                new Vector2(170, 110), new Vector2(170, 100)};
+    private Vector2[] dynamicPoints = new Vector2[]{new Vector2(100, 180), new Vector2(100, 170),
+            new Vector2(130, 140), new Vector2(130, 150)};
+
+    private PolygonShape polyShapeStatic;
+    private PolygonShape polyShapeDynamic;
+    private float desiredAngle;
+
+    private RevoluteJoint joint;
 
     public Box2DComponent() {
+
         World.setVelocityThreshold(0);
         world = new World(new Vector2(0, 9.8f * SCREEN_TO_BOX), false);
         shape = new PolygonShape();
+        createBodies();
+
+        polyShapeStatic = new PolygonShape();
+        polyShapeDynamic = new PolygonShape();
+        setBodyShape(staticPoints, polyShapeStatic, bodyA);
+        setBodyShape(dynamicPoints, polyShapeDynamic, bodyB);
+
+        RevoluteJointDef revoluteJointDef = new RevoluteJointDef();
+        revoluteJointDef.initialize(bodyA, bodyB, bodyA.getWorldCenter());
+        revoluteJointDef.collideConnected = false;
+        revoluteJointDef.localAnchorA.set(1, 3);
+        revoluteJointDef.localAnchorB.set(1, 1);
+        revoluteJointDef.enableLimit = true;
+        revoluteJointDef.lowerAngle = -30 * SCREEN_TO_BOX;
+        revoluteJointDef.upperAngle = 30 * SCREEN_TO_BOX;
+
+        joint = (RevoluteJoint) world.createJoint(revoluteJointDef);
+        joint.enableMotor(true);
+        joint.setMotorSpeed(10);
+        joint.setMaxMotorTorque(25);
 
         createBodyBox();
         createAllWalls();
-        ball1 = createBall(10);
-        ball2 = createBall(20);
-        ball3 = createBall(30);
-        time = System.currentTimeMillis();
+
+
+        this.addMouseListener(new MouseInputAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                super.mouseClicked(e);
+                Vector2 force = new Vector2(200, 100);
+                bodyB.applyForceToCenter(force, true);
+                System.out.println("a: " + bodyA.getAngle() + " b: " + bodyB.getAngle());
+                repaint();
+
+            }
+        });
     }
 
     private void createAllWalls() {
@@ -54,28 +98,34 @@ public class Box2DComponent extends JComponent {
         boxBody = world.createBody(bodyDef);
     }
 
+    private void createBodies() {
+        BodyDef bodyDef = new BodyDef();
+        bodyDef.type = BodyType.StaticBody;
+        bodyDef.position.set(50, 70);
+        bodyA = world.createBody(bodyDef);
+
+        bodyDef.type = BodyType.DynamicBody;
+        bodyDef.position.set(80, 100);
+        bodyB = world.createBody(bodyDef);
+    }
+
+    private void setBodyShape(Vector2[] points, PolygonShape polygonShape, Body body) {
+        FixtureDef fixtureDef = new FixtureDef();
+//        Vector2[] chainPoints = new Vector2[]{
+//                new Vector2(xPoints[0], yPoints[0]), new Vector2(xPoints[1], yPoints[1]),
+//                new Vector2(xPoints[2], yPoints[2]), new Vector2(xPoints[3], yPoints[3])};
+        polygonShape.set(points);
+        fixtureDef.shape = polygonShape;
+        fixtureDef.density = 1 ;
+        body.createFixture(fixtureDef);
+    }
+
     private void createWall(int hx, int hy, int x, int y) {
         FixtureDef fixtureDef = new FixtureDef();
         fixtureDef.shape = shape;
         fixtureDef.restitution = 1;
         shape.setAsBox(hx * SCREEN_TO_BOX, hy * SCREEN_TO_BOX, new Vector2(x * SCREEN_TO_BOX, y * SCREEN_TO_BOX), 0);
         boxBody.createFixture(fixtureDef);
-    }
-
-    private Body createBall(int radius) {
-        BodyDef bodyDef = new BodyDef();
-        bodyDef.type = BodyType.DynamicBody;
-        bodyDef.position.set(radius * SCREEN_TO_BOX, BOX_MARGIN * SCREEN_TO_BOX);
-        Body ball = world.createBody(bodyDef);
-
-        FixtureDef fixtureDef = new FixtureDef();
-        CircleShape circle = new CircleShape();
-        circle.setRadius(radius * SCREEN_TO_BOX);
-        fixtureDef.shape = circle;
-        fixtureDef.restitution = 1;
-        ball.createFixture(fixtureDef);
-        ball.applyForceToCenter(radius*BOX_WIDTH * SCREEN_TO_BOX, radius*BOX_WIDTH * SCREEN_TO_BOX, true);
-        return ball;
     }
 
     @Override
@@ -86,13 +136,16 @@ public class Box2DComponent extends JComponent {
             world.step((currentTime - time) / 1000f, 6, 2); //amount of millis since last time called paintComponent
             time = currentTime;
 
-            graphics.drawLine(1, BOX_WIDTH, (BOX_WIDTH - BOX_MARGIN), BOX_WIDTH);                  //bottom wall
-            graphics.drawLine((BOX_WIDTH - BOX_MARGIN), BOX_MARGIN, (BOX_WIDTH - BOX_MARGIN), BOX_WIDTH);   //right wall
-            graphics.drawLine(1, BOX_MARGIN, 1, BOX_WIDTH);                               //left wall
-            graphics.drawLine(1, BOX_MARGIN, (BOX_WIDTH - BOX_MARGIN), BOX_MARGIN);                //top wall
-            drawBall(graphics, ball1);
-            drawBall(graphics, ball2);
-            drawBall(graphics, ball3);
+            graphics.drawLine(1, BOX_WIDTH, (BOX_WIDTH - BOX_MARGIN), BOX_WIDTH);                        //bottom wall
+            graphics.drawLine((BOX_WIDTH - BOX_MARGIN), BOX_MARGIN, (BOX_WIDTH - BOX_MARGIN), BOX_WIDTH);    //right wall
+            graphics.drawLine(1, BOX_MARGIN, 1, BOX_WIDTH);                                         //left wall
+            graphics.drawLine(1, BOX_MARGIN, (BOX_WIDTH - BOX_MARGIN), BOX_MARGIN);                     //top wall
+
+
+//            graphics.drawPolygon(xPointsDynamic, yPointsDynamic, xPointsDynamic.length);
+//            graphics.drawPolygon(xPointsStatic, yPointsStatic, xPointsStatic.length);
+//            graphics.drawPolygon(polyShapeStatic);
+//            graphics.drawPolygon(polyShapeStatic, , polyShapeStatic.getVertexCount());
 
             repaint();
         }
